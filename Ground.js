c5e3ebabda82bf2df13e1331015b888bb1349874
DevarshNewar <69204233+DevarshNewar@users.noreply.github.com>
2020-10-16 10:27:15 +0530

    Add files via upload

--- Ground.js
+++ Ground.js
@@ -1,17 +1,21 @@
-class Ground{
-
-    constructor(){
-        var ground_options={
-            isStatic : true
-          }
-        
-          this.ground = Bodies.rectangle(450,390,900,20,ground_options)
-          World.add(world,this.ground);
-    }
-    display(){
-        noStroke();
-        fill(188,67,67);
-        rectMode(CENTER);
-        rect(this.ground.position.x,this.ground.position.y,900,20);
-    }
+class Ground {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true,
+    };
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+  }
+  display() {
+    var angle = this.body.angle;
+    push();
+    fill("black");
+    translate(this.body.position.x, this.body.position.y);
+    rotate(angle);
+    rectMode(CENTER);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
 }
